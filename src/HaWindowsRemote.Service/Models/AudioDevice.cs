875f8be3347209c7f38a7bf909881c9464d48e00
André Eriksen <andre.eriksen@nltg.com>
2026-02-21 10:25:31 +0100

    Add audio control via SoundVolumeView with unmute on volume change

---
+++ src/HaWindowsRemote.Service/Models/AudioDevice.cs
@@ -0,0 +1,8 @@
+namespace HaWindowsRemote.Service.Models;
+
+public sealed class AudioDevice
+{
+    public required string Name { get; init; }
+    public required int Volume { get; init; }
+    public required bool IsDefault { get; init; }
+}
